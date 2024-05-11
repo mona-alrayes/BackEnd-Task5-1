bb3e4a4abe2e32c884a271debf67751ad8155e65
mona <mona-alrayes>
2024-05-11 12:50:39 +0300

    third commit

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -16,12 +16,12 @@ class BooksSeeder extends Seeder
     public function run(): void
     {
 
-       // Retrieve or create the Fiction category and its subcategories
+       // Fiction category and its subcategories
        $fiction = Category::where('category', 'Fiction')->firstOrCreate(['category' => 'Fiction']);
        $classic = Subcategory::where('subcategory', 'Classic Literature')->firstOrCreate(['subcategory' => 'Classic Literature', 'category_id' => $fiction->id]);
        $fantasy = Subcategory::where('subcategory', 'Fantasy')->firstOrCreate(['subcategory' => 'Fantasy', 'category_id' => $fiction->id]);
        
-       // Retrieve or create the Non-fiction category and its subcategories
+       // Non-fiction category and its subcategories
        $nonFiction = Category::where('category', 'Non-fiction')->firstOrCreate(['category' => 'Non-fiction']);
        $historyNonFiction = Subcategory::where('subcategory', 'History')->firstOrCreate(['subcategory' => 'History', 'category_id' => $nonFiction->id]);
        $memoir = Subcategory::where('subcategory', 'Memoir')->firstOrCreate(['subcategory' => 'Memoir', 'category_id' => $nonFiction->id]);
@@ -37,12 +37,12 @@ class BooksSeeder extends Seeder
        $history = Category::where('category', 'History')->firstOrCreate(['category' => 'History']);
        $americanHistory = Subcategory::where('subcategory', 'American History')->firstOrCreate(['subcategory' => 'American History', 'category_id' => $history->id]);
 
-       // Create books associated with the Fiction category and its subcategories
+
        Book::create([
            'title' => 'To Kill a Mockingbird',
            'author' => 'Harper Lee',
            'description' => '"To Kill a Mockingbird" is a classic American novel that addresses themes of racial injustice, moral growth, and compassion. Set in the 1930s in the fictional town of Maycomb, Alabama, it tells the story of young Scout Finch and her father, lawyer Atticus Finch, as he defends a black man falsely accused of raping a white woman.',
-           'price' => '$12.99',
+           'price' => '12.99',
            'category_id' => $fiction->id,
            'subcategory_id' => $classic->id,
            'image' => '',
@@ -51,7 +51,7 @@ class BooksSeeder extends Seeder
             'title' => 'The Great Gatsby',
             'author' => 'F. Scott Fitzgerald',
             'description' => '"The Great Gatsby" is a novel set in the Roaring Twenties, a period of excess and decadence in America. It follows the enigmatic Jay Gatsby and his obsession with the beautiful Daisy Buchanan, exploring themes of love, wealth, and the American Dream.',
-            'price' => '$9.99',
+            'price' => '9.99',
             'category_id' => $fiction->id,
             'subcategory_id' => $classic->id,
             'image' => '',
@@ -61,20 +61,17 @@ class BooksSeeder extends Seeder
             'title' => 'The Hobbit',
             'author' => 'J.R.R. Tolkien',
             'description' => '"The Hobbit" is a fantasy novel that follows the journey of Bilbo Baggins, a hobbit who is swept into an epic quest to reclaim the lost kingdom of Erebor from the dragon Smaug. Along the way, Bilbo encounters trolls, elves, and Gollum, and discovers courage and adventure within himself.',
-            'price' => '$8.99',
+            'price' => '8.99',
             'category_id' => $fiction->id,
             'subcategory_id' => $fantasy->id,
             'image' => '',
         ]);
 
-        
-
-        // Create books associated with the Non-fiction category and its subcategories
         Book::create([
             'title' => 'Sapiens: A Brief History of Humankind',
             'author' => 'Yuval Noah Harari',
             'description' => '"Sapiens" is a non-fiction book that offers a sweeping history of the human species, from the emergence of Homo sapiens in Africa to the present day. Drawing on insights from anthropology, biology, and history, Harari explores how Homo sapiens became the dominant species on Earth and the impact of human civilization on the planet.',
-            'price' => '$15.99',
+            'price' => '15.99',
             'category_id' => $nonFiction->id,
             'subcategory_id' => $history->id,
             'image' => '',
@@ -84,7 +81,7 @@ class BooksSeeder extends Seeder
             'title' => 'Becoming',
             'author' => 'Michelle Obama',
             'description' => '"Becoming" is a memoir by former First Lady Michelle Obama, in which she reflects on her life and experiences growing up on the South Side of Chicago, her career as a lawyer and public servant, and her time in the White House. With candor and grace, Obama shares personal anecdotes and insights into her journey of self-discovery and public service.',
-            'price' => '$19.99',
+            'price' => '19.99',
             'category_id' => $nonFiction->id,
             'subcategory_id' => $memoir->id,
             'image' => '',
@@ -94,7 +91,7 @@ class BooksSeeder extends Seeder
             'title' => 'The Subtle Art of Not Giving a F*ck',
             'author' => 'Mark Manson',
             'description' => '"The Subtle Art of Not Giving a F*ck" is a self-help book that offers unconventional advice on how to live a more meaningful and fulfilling life. Drawing on psychological research and personal anecdotes, Manson argues that happiness comes from embracing life\'s struggles and accepting one\'s limitations, rather than pursuing constant positivity and success.',
-            'price' => '$13.99',
+            'price' => '13.99',
             'category_id' => $nonFiction->id,
             'subcategory_id' => $selfHelp->id,
             'image' => '',
@@ -104,7 +101,7 @@ class BooksSeeder extends Seeder
             'title' => 'A Brief History of Time',
             'author' => 'Stephen Hawking',
             'description' => '"A Brief History of Time" is a popular science book that explores complex topics such as the Big Bang theory, black holes, and the nature of time. Written for a general audience, it provides insights into the mysteries of the universe and the quest for a unified theory of physics.',
-            'price' => '$11.99',
+            'price' => '11.99',
             'category_id' => $science->id,
             'subcategory_id' => $physics->id,
             'image' => '',
@@ -114,7 +111,7 @@ class BooksSeeder extends Seeder
             'title' => 'The Selfish Gene',
             'author' => 'Richard Dawkins',
             'description' => '"The Selfish Gene" is a groundbreaking work of evolutionary biology that explores the role of genes in driving natural selection and shaping behavior. Dawkins introduces the concept of the "selfish gene," arguing that organisms are vehicles for genes to propagate themselves, leading to insights into altruism, cooperation, and the origins of life.',
-            'price' => '$14.50',
+            'price' => '14.50',
             'category_id' => $science->id,
             'subcategory_id' => $biology->id,
             'image' => '',
@@ -124,7 +121,7 @@ class BooksSeeder extends Seeder
             'title' => 'The Gene: An Intimate History',
             'author' => 'Siddhartha Mukherjee',
             'description' => '"The Gene: An Intimate History" is a comprehensive exploration of the history, science, and ethics of genetics. Mukherjee traces the discovery of the gene from ancient times to the present day, weaving together personal stories, scientific breakthroughs, and ethical dilemmas to illuminate the profound impact of genetics on our lives.',
-            'price' => '$16.99',
+            'price' => '16.99',
             'category_id' => $science->id,
             'subcategory_id' => $genetics->id,
             'image' => '',
@@ -135,7 +132,7 @@ class BooksSeeder extends Seeder
             'title' => 'A People\'s History of the United States',
             'author' => 'Howard Zinn',
             'description' => '"A People\'s History of the United States" offers a revisionist perspective on American history, focusing on the experiences of marginalized groups such as Native Americans, African Americans, and women. Zinn challenges traditional narratives of U.S. history, highlighting the struggles for justice, equality, and democracy throughout the nation\'s past.',
-            'price' => '$17.99',
+            'price' => '17.99',
             'category_id' => $history->id,
             'subcategory_id' => $americanHistory->id,
             'image' => '',
